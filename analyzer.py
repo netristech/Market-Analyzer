8f4c616aac7d3531c4c849c6aa099d3b9774e582
netristech <admin@netris.us>
2022-07-06 20:01:09 -0400

    20220706

--- analyzer.py
+++ analyzer.py
@@ -24,6 +24,7 @@ from datetime import timedelta
 def main():
     app = dash.Dash(external_stylesheets=[dbc.themes.BOOTSTRAP])
     pd.options.plotting.backend = "plotly"
+    key = "Weekly Adjusted Time Series"
     now = datetime.now()
     ten_year = now - timedelta(days=3650)
     five_year = now - timedelta(days=1825)
@@ -32,8 +33,8 @@ def main():
     six_month = now - timedelta(days=180)
     three_month = now - timedelta(days=90)
     one_month = now - timedelta(days=30)
-    two_week = now - timedelta(minutes=20160)
-    one_week = now - timedelta(minutes=10080)
+    two_week = now - timedelta(days=14)
+    one_week = now - timedelta(days=7)
 
     sidebar = dbc.Col([
         dbc.FormGroup([
@@ -42,15 +43,9 @@ def main():
                 id='watch-tickers',
                 placeholder='Enter symbol(s)',
                 type='text',
+                pattern='[A-Za-z]{1,6}',
                 value=''
-            ),
-            html.Label("Enter symbol(s) you own."),
-            dcc.Input(
-                id='own-tickers',
-                placeholder='Enter symbol(s)',
-                type='text',
-                value=''
-            ),            
+            ),           
             dcc.Slider(
                 id="term-slider",
                 min=1,
@@ -90,14 +85,13 @@ def main():
             is_open=False,
             dismissable=True
         ),
-        html.Div(id="data", style={"display": "none"}),
         dcc.Interval(
             id="interval-component",
             interval=60000,
             n_intervals=0
         ),        
-        html.Div(id="ticker"),
         html.H3(id="time", className="text-center"),
+        html.Div(id="test"),
         #dcc.Graph(id="time-series-chart"),      
         #dbc.Row([
         #    dbc.Col(id="signal", md=3),
@@ -108,41 +102,66 @@ def main():
     
     app.layout = dbc.Container([
         dbc.Row([sidebar, content],
-        className="text-dark")
+        className="text-dark"),
+        dcc.Store(id="data")
     ], id="container", fluid=True)
 
+    # Update time and display
     @app.callback(
         Output("time", "children"),
         Input("interval-component", "n_intervals")
     )
     def update_time(n):
-        return datetime.now().strftime('%m/%d/%Y %H:%M')       
+        return datetime.now().strftime('%m/%d/%Y %H:%M')
 
-    # Get time-series data from API and update Dash Bootstrap components
     @app.callback(
-        Output("data", "children"),
-        Output("alert", "children"),
+        Output("alert", "content"),
         Output("alert", "is_open"),
         Input("lookup-btn", "n_clicks"),
         State("watch-tickers", "value"),
-    )    
-    def get_data(n_clicks, ticker):
-        if len(ticker) < 1 or len(ticker) > 6:
-            return dash.no_update, f"Invalid string length ({ticker}): must be between 1 and 6 characters", True
-        if not ticker.isalpha():
-            return dash.no_update, f"Invalid input type ({ticker}): must be letters only", True
-        try:
-            return format_data(requests.get(f"https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol={ticker}&outputsize=full&apikey=9LVE9OGAKH31RPWM&datatype=json")), dash.no_update, False
-        except:
-            return dash.no_update, f"Invalid symbol: {ticker}", True
+    )
+    def check_input(n_clicks, tickers):
+        for ticker in tickers.split():
+            if not ticker.isalpha():
+                return "Invalid input: tickers should only contain letters", True
+            if len(ticker) < 1 or len(ticker) > 5:
+                return "Invalid input: ticker length should be between 1 and 5 characters", True
+            
 
-    # Format data and return as Pandas DataFram object             
+'''
+    # Get time-series data from API and update Dash Bootstrap components
+    @app.callback(
+        Output("data", "data"),
+        Input("lookup-btn", "n_clicks"),
+        State("watch-tickers", "value"),
+    )
+    def check_input(n_clicks, tickers):
+        
+    def get_data(n_clicks, tickers):
+        data = {}
+        for ticker in tickers.replace(',', ' ').split():
+            resp = requests.get(f"https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol={ticker}&outputsize=full&apikey=9LVE9OGAKH31RPWM&datatype=json")
+            if "Error Message" in json.loads(resp.content):
+                return json.dumps({"error": "invalid input"})
+            else:
+                data.update({ticker: json.loads(resp.content)})
+        return json.dumps(data)'''
+    
+    # Debugging output - REMOVE LATER
+    @app.callback(
+        Output("test", "children"),
+        Input("data", "data")
+    )
+    def print_data(data):
+        return data
+
+    # Format API data and return as Pandas DataFram object; expects requests response as input; returns Pandas DataFrame as JSON
     def format_data(resp):
         data = json.loads(resp.content)
         dates, vals = ([] for i in range(2))
-        for i in data["Weekly Adjusted Time Series"]:
+        for i in data[key]:
             dates.append(i)
-            vals.append(data["Weekly Adjusted Time Series"][i]["5. adjusted close"])
+            vals.append(data[key][i]["5. adjusted close"])
         return pd.DataFrame(dict(date=dates, value=vals)).to_json(date_format='iso', orient='split')
 
     app.run_server(port='8080', debug=True)
