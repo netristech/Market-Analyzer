0ccfb2740ff64c4afd5d9df844b184cd830ca325
netristech <admin@netris.us>
2024-04-25 22:40:51 -0400

    Testing faster macd periods

--- analyzer.py
+++ analyzer.py
@@ -333,7 +333,7 @@ def main():
         signal = macd.ewm(span=sig, min_periods=sig).mean()
         df['macd'] = df.index.map(macd)
         df['signal'] = df.index.map(signal)
-        mean_macd = macd.rolling(slow * 4).mean()
+        mean_macd = macd.rolling(slow * 10).mean()
         df['mean_macd'] = df.index.map(mean_macd)
     
     def get_macd_median(df, period=90):
